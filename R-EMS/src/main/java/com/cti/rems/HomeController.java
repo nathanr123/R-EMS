664d7cee82b6e583ce460bbd8dcca66547280d15
nathanr123 <nathanr.kamal@gmail.com>
2014-08-02 14:54:15 +0530

    Spring Security Integrated

--- R-EMS/src/main/java/com/cti/rems/HomeController.java
+++ R-EMS/src/main/java/com/cti/rems/HomeController.java
@@ -1,5 +1,6 @@
 package com.cti.rems;
 
+import java.security.Principal;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,25 +18,73 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model, Principal principal) {
+
+		System.out
+				.println("*************Welcome Controller Called**************");
+
+		String name = principal.getName();
+
+		model.addAttribute("message",
+				"Spring Security Successfully Integrated!!!!!!!!!!!");
+
+		model.addAttribute("username", name);
+
+		return "hello";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+
+		System.out
+				.println("*************Login Controller Called**************");
+
+		return "login";
+	}
+
+	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+
+		System.out
+				.println("*************Loginfailed Controller Called**************");
+
+		model.addAttribute("error", "true");
+
+		return "login";
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+
+		System.out
+				.println("*************Logout Controller Called**************");
+
+		return "login";
+	}
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/*", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
+
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
 }
